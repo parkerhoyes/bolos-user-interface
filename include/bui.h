9862e25784470d1d7d9d50a3e205cf58dd6bc5a0
Parker Hoyes <contact@parkerhoyes.com>
2017-01-05 14:41:49 -0500

    Externalized display buffer
    The display buffers that were originally kept inside bui.c have been externalized. Now, callers of the API must allocate their own display buffer and pass it to the rendering / displaying functions. This saves memory because it allows application to opt to use just one display buffer instead of two in the previous double-buffered design.

--- include/bui.h
+++ include/bui.h
@@ -28,6 +28,12 @@
 
 #include "bui_font.h"
 
+typedef struct bui_bitmap_128x32_t {
+	// A 128x32 bitmap. Every 128 bits is a row ordered from bottom to top, each row containing 128 pixels ordered from
+	// right to left on the screen. The foreground color is represented by 1 bits, and the background color by 0 bits.
+	unsigned char bitmap[512];
+} bui_bitmap_128x32_t;
+
 /*
  * Bitmaps in this library are represented as sequences of bits, each representing the color of a single pixel (a 1 bit
  * represents the foreground color, and a 0 bit represents the background color). Bitmaps are stored big-endian (first
@@ -59,83 +65,69 @@ BUI_DECLARE_BITMAP(badge_cross);
 BUI_DECLARE_BITMAP(badge_dashboard);
 
 /*
- * Initialize various data structures used by the UI manager. This should always be called once before any other
- * function in this API is called. This function fills the top and bottom display buffers with the background color.
- */
-void bui_init();
-
-/*
- * Send additional data contained within the top display buffer to the MCU if available, otherwise do nothing.
- */
-void bui_display();
-
-/*
- * Indicate to the UI manager that the MCU has processed the last display status sent. This functions should only and
- * always be called after SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT is triggered.
- */
-void bui_display_processed();
-
-/*
- * Transfer the bottom display buffer onto the top display buffer. The bottom buffer is the buffer that may be drawn
- * onto by the application, and the top buffer is the one in the process of being displayed on the screen. Calling this
- * function before the top buffer is fully displayed is legal.
- */
-void bui_flush();
-
-/*
- * Determine if the UI manager is still in the process of sending the top display buffer to the MCU to be displayed.
- *
- * Returns:
- *     false if the UI manager is in the process of sending the top display buffer to the MCU to be displayed, true
- *     otherwise
- */
-bool bui_display_done();
-
-/*
- * Fill the bottom display buffer with the specified color.
+ * Send some data contained within the provided display buffer to the MCU to be displayed.
  *
  * Args:
- *     color: the color with which to fill the bottom display buffer; true is the foreground color and false is the
- *            background color
+ *     buffer: the display buffer to be displayed
+ *     progress: the progress made displaying this buffer; this should be 0 to start displaying the buffer anew, or a
+ *               value previously returned by bui_display(...), but not -1
+ * Returns:
+ *     the progress made displaying the buffer, or -1 if the buffer has been completely displayed
  */
-void bui_fill(bool color);
+signed char bui_display(bui_bitmap_128x32_t *buffer, signed char progress);
 
 /*
- * Invert every pixel in the bottom display buffer.
+ * Fill the provided display buffer with the specified color.
+ *
+ * Args:
+ *     buffer: the display buffer to be filled
+ *     color: the color with which to fill the display buffer; true is the foreground color and false is the background
+ *            color
  */
-void bui_invert();
+void bui_fill(bui_bitmap_128x32_t *buffer, bool color);
 
 /*
- * Fill a rectangle on the bottom display buffer. Any part of the rectangle out of bounds of the display will not be
+ * Invert every pixel in the provided display buffer.
+ *
+ * Args:
+ *     buffer: the display buffer to be inverted
+ */
+void bui_invert(bui_bitmap_128x32_t *buffer);
+
+/*
+ * Fill a rectangle on the provided display buffer. Any part of the rectangle out of bounds of the display will not be
  * drawn. If the width or height is 0, this function has no side effects.
  *
  * Args:
+ *     buffer: the display buffer onto which to draw the rectangle
  *     x: the x-coordinate of top-left corner of the destination rectangle; must be >= -32,768 and <= 32,767
  *     y: the y-coordinate of top-left corner of the destination rectangle; must be >= -32,768 and <= 32,767
  *     w: the width of the rectangle; must be >= 0 and <= 32,767
  *     h: the height of the rectangle; must be >= 0 and <= 32,767
  *     color: the color with which to fill the rectangle; true is the foreground color and false is the background color
  */
-void bui_fill_rect(int x, int y, int w, int h, bool color);
+void bui_fill_rect(bui_bitmap_128x32_t *buffer, int x, int y, int w, int h, bool color);
 
 /*
- * Set the color of a single pixel on the bottom display buffer. If the coordinates of the pixel are out of bounds of
+ * Set the color of a single pixel on the provided display buffer. If the coordinates of the pixel are out of bounds of
  * the display, this function has no side effects.
  *
  * Args:
+ *     buffer: the display buffer whose pixel is to be set
  *     x: the x-coordinate of the pixel to be set
  *     y: the y-coordinate of the pixel to be set
  *     color: the color to which the pixel is to be set; true is the foreground color and false is the background color
  */
-void bui_set_pixel(int x, int y, bool color);
+void bui_set_pixel(bui_bitmap_128x32_t *buffer, int x, int y, bool color);
 
 /*
- * Draw a bitmap onto the bottom display buffer given a source rectangle on the bitmap's coordinate plane and a
- * destination rectangle on the display's coordinate plane. Any part of the destination rectangle out of bounds of the
- * display will not be drawn. The source rectangle must be entirely within the source bitmap. If the width or height is
+ * Draw a bitmap onto the provided display buffer given a source rectangle on the bitmap's coordinate plane and a
+ * destination rectangle on the buffer's coordinate plane. Any part of the destination rectangle out of bounds of the
+ * buffer will not be drawn. The source rectangle must be entirely within the source bitmap. If the width or height is
  * 0, this function has no side effects.
  *
  * Args:
+ *     buffer: the display buffer onto which to draw the bitmap
  *     bitmap: the pointer to the bitmap to be drawn; for each pixel, true is the foreground color and false is the
  *             background color
  *     bitmap_w: the number of pixels in a single row in the bitmap; must be >= 0 and <= 32,767
@@ -143,15 +135,15 @@ void bui_set_pixel(int x, int y, bool color);
  *            plane; must be >= 0 and <= 32,767
  *     src_y: the y-coordinate of the top-left corner of the source rectangle within the source bitmap's coordinate
  *            plane; must be >= 0 and <= 32,767
- *     dest_x: the x-coordinate of the top-left corner of the destination rectangle on or outside of the display's
+ *     dest_x: the x-coordinate of the top-left corner of the destination rectangle on or outside of the buffer's
  *             coordinate plane; must be >= -32,768 and <= 32,767
- *     dest_y: the y-coordinate of the top-left corner of the destination rectangle on or outside of the display's
+ *     dest_y: the y-coordinate of the top-left corner of the destination rectangle on or outside of the buffer's
  *             coordinate plane; must be >= -32,768 and <= 32,767
  *     w: the width of the source and destination rectangles; must be >= 0 and <= 32,767
  *     h: the height of the source and destination rectangles; must be >= 0 and <= 32,767
  */
-void bui_draw_bitmap(const unsigned char *bitmap, int bitmap_w, int src_x, int src_y, int dest_x, int dest_y, int w,
-		int h);
+void bui_draw_bitmap(bui_bitmap_128x32_t *buffer, const unsigned char *bitmap, int bitmap_w, int src_x, int src_y,
+		int dest_x, int dest_y, int w, int h);
 
 /*
  * Draw a character in the specified font onto the bottom display buffer. Any part of the character out of bounds of the
@@ -162,6 +154,7 @@ void bui_draw_bitmap(const unsigned char *bitmap, int bitmap_w, int src_x, int s
  * the character's boundaries are considered to extend from the font's baseline to the ascender height.
  *
  * Args:
+ *     buffer: the display buffer onto which to draw the character
  *     ch: the character code of the character to be drawn
  *     x: the x-coordinate of the text anchor; must be >= -32,768 and <= 32,767
  *     y: the y-coordinate of the text anchor; must be >= -32,768 and <= 32,767
@@ -170,7 +163,8 @@ void bui_draw_bitmap(const unsigned char *bitmap, int bitmap_w, int src_x, int s
  *                the form BUI_VERTICAL_ALIGN_<TOP|CENTER|BOTTOM>
  *     font_id: the ID of the font to be used to render the character
  */
-void bui_draw_char(unsigned char ch, int x, int y, unsigned char alignment, bui_font_id_e font_id);
+void bui_draw_char(bui_bitmap_128x32_t *buffer, unsigned char ch, int x, int y, unsigned char alignment,
+		bui_font_id_e font_id);
 
 /*
  * Draw a string in the specified font onto the bottom display buffer. Any part of the string out of bounds of the
@@ -182,6 +176,7 @@ void bui_draw_char(unsigned char ch, int x, int y, unsigned char alignment, bui_
  * ascender height.
  *
  * Args:
+ *     buffer: the display buffer onto which to draw the string
  *     str: the null-terminated string to be drawn; may not be NULL
  *     x: the x-coordinate of the text anchor; must be >= -32,768 and <= 32,767
  *     y: the y-coordinate of the text anchor; must be >= -32,768 and <= 32,767
@@ -190,6 +185,7 @@ void bui_draw_char(unsigned char ch, int x, int y, unsigned char alignment, bui_
  *                the form BUI_VERTICAL_ALIGN_<TOP|CENTER|BOTTOM>
  *     font_id: the ID of the font to be used to render the string
  */
-void bui_draw_string(const unsigned char *str, int x, int y, unsigned char alignment, bui_font_id_e font_id);
+void bui_draw_string(bui_bitmap_128x32_t *buffer, const unsigned char *str, int x, int y, unsigned char alignment,
+		bui_font_id_e font_id);
 
 #endif
